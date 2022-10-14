e2a20f7e589556fe8034e1bcfd8931b7b83e36a3
Emmanuel Awolowo <awolowojamb@gmail.com>
2022-10-13 21:44:16 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,3 @@
+#!/bin/bash
+printf("Size of char: %zu bytes\n", sizeof(charType)); printf("Size of int: %zu bytes\n", sizeof(intType)); printf("Size of long int: %zu bytes\n", sizeof(longintType)); printf(Size of long long int: %zu bytes\n", sizeof(longlongintType)); printf("Size of float: %zu bytes\n", sizeof(floatType)); return 0;
+
